0ad183eca62625febf7f9e6952c378cf42c2e71b
Lefika <klefika@deloitte.co.za>
2026-08-05 11:27:19 +0200

    Removing the back end

--- backend/app/routes/queue.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.services.queue_service import load_queue
-
-router = APIRouter()
-
-@router.get("/queue")
-def get_queue():
-    return load_queue()
--- backend/app/services/analyzer.py
+++
@@ -1,6 +0,0 @@
-from app.models.candidate import Candidate
-from app.services.bedrock import analyze_cv_with_bedrock
-
-def analyze_cv(text:str) -> Candidate:
-    result = analyze_cv_with_bedrock(text)
-    return Candidate(**result)
